25974d2663b641c3dfc06450ed7d48b13862eea1
Kevin Mulligan <kevin@kevin-mulligan.org>
2023-02-25 14:31:38 -0500

    removed extra functions

--- app/main.py
+++ app/main.py
@@ -29,16 +29,6 @@ while True:
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "fav foods", "content" : "I like pizza", "id": 2}]
 
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-        
-def find_post_index(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
 @app.get("/")
 def root():
     return {"message": "welcome to my api"}
