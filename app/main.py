63da282ea086774ffac1257a73528275a4aafdc6
Kevin Mulligan <kevin@kevin-mulligan.org>
2023-03-09 07:38:24 -0500

    Added authentication and user based functionality

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from ast import Delete
 from typing import Optional, List
 from typing_extensions import deprecated
 from fastapi import FastAPI, Response, status, HTTPException, Depends
@@ -10,7 +9,7 @@ from time import sleep
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, SessionLocal, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -33,10 +32,11 @@ my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
     return {"message": "welcome to my api"}
 
-
+# try to fix this
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,19 +1,6 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    
-class PostCreate(PostBase):
-    pass
-
-class Post(PostBase):
-    id: int
-    created_on: datetime
-    class Config:
-        orm_mode = True
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -25,3 +12,31 @@ class UserOut(BaseModel):
 
     class Config:
         orm_mode = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+        
+class TokenData(BaseModel):
+    id: Optional[str] = None
+    
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    created_on: datetime
+    owner_id: int
+    owner: UserOut
+    class Config:
+        orm_mode = True
+
